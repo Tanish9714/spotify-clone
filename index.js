343032e771c7da44ba2aed6d18b08a7052ac847c
Tanish9714 <146628920+Tanish9714@users.noreply.github.com>
2023-10-01 15:35:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let songIndex = 0;
-let audioElement = new Audio('/songs/1.mp3');
+let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
